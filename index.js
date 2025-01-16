b71f6a24fda52db90227992856b54fa7805c198e
Abby <abbi990>
2025-01-16 09:46:06 +0100

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const nav = document.querySelector('.main-nav .nav');
+    nav.classList.toggle('show');
+  }
+
+  .scrolling-text-container:hover .scrolling-text {
+    animation-play-state: paused;
+  }
+  
